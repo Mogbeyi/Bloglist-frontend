331652a85f0b1af7f2a5b46d70c20dccaf247e27
Mogbeyi <mogbeyiwenyinmi@gmail.com>
2021-06-23 11:42:52 +0100

    Fix username login issue

--- src/App.js
+++ src/App.js
@@ -1,45 +1,64 @@
 import React, { useState, useEffect } from "react";
 import Blog from "./components/Blog";
 import blogService from "./services/blogs";
+import loginService from "./services/login";
 
 const App = () => {
   const [blogs, setBlogs] = useState([]);
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
     blogService.getAll().then((blogs) => setBlogs(blogs));
   }, []);
 
-  const handleLogin = (event) => {
+  const handleLogin = async (event) => {
     event.preventDefault();
-    console.log("loggin in with", username, password);
+
+    let loginUser = {
+      "username": username.toLowerCase(),
+      "password": password
+    }
+
+    try {
+      const user = await loginService.login(loginUser);
+      setUser(user);
+      setUsername("");
+      setPassword("");
+    } catch (exception) {}
   };
+
+  if (user === null) {
+    return (
+      <div>
+        <form onSubmit={handleLogin}>
+          <div>
+            username
+            <input
+              type="text"
+              value={username}
+              name="Username"
+              onChange={({ target }) => setUsername(target.value)}
+            />
+          </div>
+          <div>
+            password
+            <input
+              type="text"
+              value={password}
+              name="password"
+              onChange={({ target }) => setPassword(target.value)}
+            />
+          </div>
+          <button type="submit">login</button>
+        </form>
+      </div>
+    );
+  }
 
   return (
     <div>
-      <form onSubmit={handleLogin}>
-        <div>
-          username
-          <input
-            type="text"
-            value={username}
-            name="Username"
-            onChange={({ target }) => setUsername(target.value)}
-          />
-        </div>
-
-        <div>
-          password
-          <input
-            type="text"
-            value={password}
-            name="password"
-            onChange={({ target }) => setPassword(target.value)}
-          />
-        </div>
-      </form>
-
       <h2>blogs</h2>
       {blogs.map((blog) => (
         <Blog key={blog.id} blog={blog} />
--- src/services/blogs.js
+++ src/services/blogs.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 const baseUrl = "http://localhost:3003/api/blogs";
 
-const getAll = () => {
-  const request = axios.get(baseUrl);
-  return request.then((response) => response.data);
+const getAll = async () => {
+  const request = await axios.get(baseUrl);
+  return request.data;
 };
 
 export default { getAll };
