9819f35fa5ba2a07f0d73741b16c8efb51757dd0
Mogbeyi <mogbeyiwenyinmi@gmail.com>
2021-07-05 16:26:38 +0100

    Sort post in ascending number of likes Complete exercise 5.9

--- src/App.js
+++ src/App.js
@@ -15,7 +15,11 @@ const App = () => {
   const [loggedOut, setLoggedOut] = useState(false);
 
   useEffect(() => {
-    blogService.getAll().then((blogs) => setBlogs(blogs));
+    blogService
+      .getAll()
+      .then((blogs) =>
+        setBlogs(blogs.sort((a, b) => (a.likes > b.likes ? 1 : -1)))
+      );
   }, []);
 
   useEffect(() => {
